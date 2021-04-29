c141a0eab9bd83ecc4d7d9ab34fd5b83428f49c0
Marco <m.j.deken@student.vu.nl>
2021-04-29 14:44:49 +0200

    Improved code readability of GFE class

--- estimate-Bon_Man.py
+++ estimate-Bon_Man.py
@@ -7,14 +7,11 @@ Created on Sun Apr 11 16:03:51 2021
 
 import pandas as pd
 import numpy as np
-from scipy.linalg import lstsq
-from lib.tracktime import TrackTime, TrackReport
-from sklearn import linear_model
-import statsmodels.api as sm
 from copy import copy
-# import enum
+from scipy.linalg import lstsq
 
 from simulate import Effects, Slopes, Variance, Dataset
+from lib.tracktime import TrackTime, TrackReport
 
 
 class GFE:
@@ -40,9 +37,6 @@ class GFE:
                 self.alpha[g,:] = (self.Y[start:end] - self.X.values[start:end] @ self.theta).reshape(self.T)
             else:
                 self.alpha[g,:] = (self.Y[start:end] - self.X.values[start:end] @ self.theta[:,g].reshape(self.K,1)).reshape(self.T)
-
-        self.initial_theta = copy(self.theta)
-        self.initial_alpha = copy(self.alpha)
 
         self.groups = np.zeros((self.N,1), dtype=int)
 
@@ -79,7 +73,6 @@ class GFE:
                 slope_dummies[i*self.T + t] = self.groups[i]
 
         if self.slopes == Slopes.heterog:
-            # TrackTime("Slope dummies")
             new_x = np.zeros((self.N*self.T, self.K*self.G))
             slope_dummies = pd.get_dummies(slope_dummies).values
 
@@ -89,13 +82,10 @@ class GFE:
                 new_x[:, counter*self.G : (counter+1)*self.G] = slope_dummies * self.X[feature].values.reshape(-1,1)
             X = pd.DataFrame(new_x)
         else:
-            # TrackTime("Copy new X")
             X = copy(self.X)
 
         X['alpha'] = effect_dummies
-
-        # TrackTime("get_dummies()")
-        X = pd.get_dummies(X)#, drop_first=True)
+        X = pd.get_dummies(X)
         return X
 
 
@@ -125,21 +115,19 @@ class GFE:
         self.Y = Y.values.reshape(N*T,1)
 
         self._initial_values()
-        # print(self.theta)
-        # print(self.alpha)
         prev_theta = np.zeros_like(self.theta)
 
-        for s in range(250):
-            # TrackTime("Fit a group")
+        for s in range(100):
+            TrackTime("Fit a group")
             self._group_assignment()
 
-            # TrackTime("Make dummy labels")
+            TrackTime("Make dummy labels")
             X = self._prepare_dummy_dataset()
 
-            # TrackTime("lstsq()")
+            TrackTime("Least Squares")
             p, _, _, _ = lstsq(X, self.Y)
 
-            # TrackTime("Update values")
+            TrackTime("Estimate")
             self._update_values(p, X.columns)
 
             # print((self.theta-prev_theta)**2)
@@ -147,143 +135,45 @@ class GFE:
                 break
             prev_theta = copy(self.theta)
 
-        print("TOOK %s ITERATIONS\n"%(s+1))
-        print("ESTIMATED COEFFICIENTS:")
-        print(self.theta)
-        print("\n")
+        self.nr_iterations = s+1
+
+        #TODO: order group numbers based on slope (including fixed effects)
+        if self.slopes == Slopes.heterog:
+            self.theta = np.sort(self.theta, axis=1)
+
+
+
+np.random.seed(0)
+N = 250
+T = 10
+K = 6
 
 
 TrackTime("Simulate")
+dataset = Dataset(N, T, K, G=4)
+dataset.simulate(Effects.ind_fix, Slopes.heterog, Variance.homosk)
 
-# np.random.seed(0)
-N = 250
-T = 10
-K = 4
 
-# gfe = GFE(N, T, K)
-
-dataset = Dataset(N, T, K, G=3)
-dataset.simulate(Effects.gr_tvar_fix, Slopes.heterog, Variance.homosk)
-#TODO: Bonhomme and Manresa estimation
-
-TrackTime("Initialize")
+TrackTime("Estimate")
 G = dataset.G   #assume true value of G is known
 
 x = dataset.data.drop(["y"], axis=1)
-y = dataset.data["y"]#.values.reshape(N*T,1)
+y = dataset.data["y"]
 
-TrackTime("class")
 gfe = GFE(Slopes.heterog, Effects.gr_tvar_fix)
 gfe.estimate_G(dataset.G)
 gfe.fit(x, y)
 
 
-TrackTime("original")
-y = y.values.reshape(N*T,1)
-heterog = True
 
-#initialize feasible values:
-theta = np.random.uniform(0.5, 5, size=(K, G))  # slopes
-alpha = np.zeros((G,T))
+TrackTime("Estimate")
 
-choices = np.random.choice(range(N), (G), replace=False)
-for i in range(G):
-    n = choices[i]
-    if not heterog:
-        alpha[i,:] = (y[n*T:(n+1)*T] - x.values[n*T:(n+1)*T] @ theta).reshape(T)
-    else:
-        alpha[i,:] = (y[n*T:(n+1)*T] - x.values[n*T:(n+1)*T] @ theta[:,i].reshape(K,1)).reshape(T)
+groups_list = [[] for g in range(G)]
+for i in range(N):
+    groups_list[gfe.groups[i][0]].append(i)
 
 
-groups = np.zeros((N,1), dtype=int)
-prev_theta = np.zeros_like(theta)
-
-
-theta = gfe.initial_theta
-alpha = gfe.initial_alpha
-# print(theta)
-# print(alpha)
-
-for s in range(250):
-    unused_g = set(range(G))
-    # TrackTime("Fit a group")
-    x = dataset.data.drop(["y"], axis=1)
-
-    for i in range(N):
-        best_g_val = np.Inf
-        if not heterog:
-            resids = y[i*T:(i+1)*T] - x.values[i*T:(i+1)*T]@theta
-
-        for g in range(G):
-            if heterog:
-                resids = y[i*T:(i+1)*T] - x.values[i*T:(i+1)*T] @ theta[:,g].reshape(K,1)
-
-            temp = (resids.reshape(T) - alpha[g,:]).reshape(-1,1)
-
-            fit = temp.T@temp
-            if fit < best_g_val:
-                best_g_val = fit
-                groups[i] = g
-
-
-        unused_g.discard(groups[i][0])
-
-    # TrackTime("Make dummy labels")
-
-    effect_dummies = np.zeros(N*T, dtype=object)
-    slope_dummies = np.zeros(N*T, dtype=int)
-    for i in range(N):
-        for t in range(T):
-            effect_dummies[i*T + t] = "%d_%d" %(groups[i],t)
-            slope_dummies[i*T + t] = groups[i]
-
-    if heterog:
-        # TrackTime("Slope dummies")
-        new_x = np.zeros((N*T,K*G))
-        slope_dummies = pd.get_dummies(slope_dummies).values
-    
-        counter = -1
-        for feature in x.columns:
-            counter += 1
-            new_x[:,counter*G:(counter+1)*G] = slope_dummies * x[feature].values.reshape(-1,1)
-        x = pd.DataFrame(new_x)
-    
-    x['alpha'] = effect_dummies
-
-    # TrackTime("get_dummies()")
-    x = pd.get_dummies(x)#, drop_first=True)
-
-    # TrackTime("lstsq()")
-    p, _, _, _ = lstsq(x, y)
-
-    # TrackTime("Update values")
-
-    index = K*G if heterog else K
-    if heterog:
-        theta = p[:index].reshape(K,G)
-    else:
-        theta = p[:index]
-
-    # print((theta-prev_theta)**2)
-
-    i = 0
-    for g in x.columns[index::T]:
-        g = int(g.split("_")[1])
-        alpha[g,:] = p[index + i*T : index + (i+1)*T].reshape(T)
-        i += 1
-
-    if np.all((theta-prev_theta)**2 < 0.00001):
-        break
-    prev_theta = theta
-
-
-TrackTime("Conclusion")
-
-# groups_list = [[] for g in range(G)]
-# for i in range(N):
-#     groups_list[groups[i][0]].append(i)
-
-print("TOOK %s ITERATIONS\n"%(s+1))
+print("TOOK %s ITERATIONS\n"%gfe.nr_iterations)
 
 print("TRUE COEFFICIENTS:")
 print(dataset.slopes_df)
@@ -291,28 +181,32 @@ print(dataset.slopes_df)
 # print(dataset.groups_list)
 
 print("\n\nESTIMATED COEFFICIENTS:")
-print(theta)
+print(gfe.theta)
 # print(alpha)
 # print(groups_list)
 
 
-exog = dataset.data.drop(['y'], axis=1)
-endog = dataset.data['y']
 
-# from linearmodels import PooledOLS
-# mod = PooledOLS(endog, exog) 
-# pooledOLS_res = mod.fit(cov_type='clustered', cluster_entity=True)
-# print("\nPOOLED OLS ESTIMATION:"), print(pooledOLS_res.params)
+if gfe.slopes == Slopes.homog:
+    TrackTime("Standard libraries")
+    exog = dataset.data.drop(['y'], axis=1)
+    endog = dataset.data['y']
+    
+    # from linearmodels import PooledOLS
+    # mod = PooledOLS(endog, exog) 
+    # pooledOLS_res = mod.fit(cov_type='clustered', cluster_entity=True)
+    # print("\nPOOLED OLS ESTIMATION:"), print(pooledOLS_res.params)
+    
+    from linearmodels import RandomEffects
+    model_re = RandomEffects(endog, exog)
+    re_res = model_re.fit()
+    print("\nRANDOM EFFECTS ESTIMATION:"), print(re_res.params)
+    
+    from linearmodels import PanelOLS
+    model_fe = PanelOLS(endog, exog, entity_effects = True)
+    fe_res = model_fe.fit()
+    print("\nFIXED EFFECTS ESTIMATION:"), print(fe_res.params)
 
-# from linearmodels import RandomEffects
-# model_re = RandomEffects(endog, exog)
-# re_res = model_re.fit()
-# print("\nRANDOM EFFECTS ESTIMATION:"), print(re_res.params)
-
-# from linearmodels import PanelOLS
-# model_fe = PanelOLS(endog, exog, entity_effects = True)
-# fe_res = model_fe.fit()
-# print("\nFIXED EFFECTS ESTIMATION:"), print(fe_res.params)
 
 print("\n")
 TrackReport()
