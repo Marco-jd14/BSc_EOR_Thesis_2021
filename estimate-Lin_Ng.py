f6fceaf54cc7f3d9ef455d0affb5fcd0819507bd
Marco <m.j.deken@student.vu.nl>
2021-05-03 00:38:37 +0200

    Implemented estimation for G>2 and G powers of two

--- estimate-Lin_Ng.py
+++ estimate-Lin_Ng.py
@@ -17,36 +17,42 @@ class PSEUDO:
         pass
 
 
-    def _cluster_regressor(self, k, q_hat):
-        gamma_range = np.zeros(N-1-2*(self.min_group_size-1))
+    def _cluster_regressor(self, k, q_hat, left_n):
+        gamma_star = [np.Inf, np.Inf, 0, 0]
+        #TODO: check if enough individuals w.r.t min_group_size
+        gamma_range = np.zeros(len(q_hat)-1-2*(self.min_group_size-1))
         q_hat_sorted = np.sort(q_hat)
 
         for i in range(len(gamma_range)):
             gamma_range[i] = (q_hat_sorted[self.min_group_size-1+i] + q_hat_sorted[self.min_group_size+i])/2
 
         for gamma in gamma_range:
-            ssr = self._ssr(gamma, q_hat)
+            ssr_left, ssr_right = self._ssr(gamma, q_hat, left_n)
             TrackTime("Calc gamma_star")
-            if ssr < self.gamma_star[0]:
-                self.gamma_star[0] = ssr
-                self.gamma_star[1] = gamma
-                self.gamma_star[2] = k
+            if ssr_left + ssr_right < gamma_star[0] + gamma_star[1]:
+                gamma_star[0] = ssr_left
+                gamma_star[1] = ssr_right
+                gamma_star[2] = gamma
+                gamma_star[3] = k
+
+        return gamma_star
 
 
-    def _ssr(self, gamma, q_hat):
-        TrackTime("gamma SSR")
-        partition = [np.arange(self.N)[q_hat <= gamma].reshape(1,-1)[0], np.arange(self.N)[q_hat > gamma].reshape(1,-1)[0]]
+    def _ssr(self, gamma, q_hat, left_n):
+        TrackTime("Partition indices1")
+        partition = [np.arange(len(q_hat))[q_hat <= gamma]+left_n, np.arange(len(q_hat))[q_hat > gamma]+left_n]
 
-        TrackTime("SSR selection")
-        partition2 = [np.zeros(len(partition[0])*self.T,dtype=int), np.zeros(len(partition[1])*self.T, dtype=int)]
+        TrackTime("Partition indices2")
+        partition_indices = [np.zeros(len(partition[0])*self.T,dtype=int), np.zeros(len(partition[1])*self.T, dtype=int)]
         for k in range(2):
             for i in range(len(partition[k])):
-                partition2[k][i*T:(i+1)*T] = np.arange(partition[k][i]*T,(partition[k][i]+1)*T)
+                partition_indices[k][i*self.T:(i+1)*self.T] = np.arange(self.T) + partition[k][i]*self.T
 
-        x1 = self.X.values[partition2[0],:] # ~35x faster than  x1 = self.X.loc[partition[0],:]
-        x2 = self.X.values[partition2[1],:]
-        y1 = self.Y.values[partition2[0]]
-        y2 = self.Y.values[partition2[1]]
+        TrackTime("Partition selection")
+        x1 = self.X.values[partition_indices[0],:] # ~35x faster than  x1 = self.X.loc[partition[0],:]
+        x2 = self.X.values[partition_indices[1],:]
+        y1 = self.Y.values[partition_indices[0]]
+        y2 = self.Y.values[partition_indices[1]]
 
         TrackTime("gamma SSR")
         beta_hat_1, _, _, _ = lstsq(x1, y1)
@@ -55,7 +61,42 @@ class PSEUDO:
         residuals1 = y1 - x1 @ beta_hat_1
         residuals2 = y2 - x2 @ beta_hat_2
 
-        return residuals1@residuals1.T + residuals2@residuals2.T
+        return residuals1@residuals1.T, residuals2@residuals2.T
+
+
+    def _calc_k_star(self, gamma_stars):
+        k_star = [np.Inf, 0]
+        for k in range(self.K):
+            tot_ssr = gamma_stars[k][0][0]
+            for gamma in gamma_stars[k]:
+                tot_ssr += gamma[1]
+
+            if tot_ssr < k_star[0]:
+                k_star[0] = tot_ssr
+                k_star[1] = k
+
+        return k_star[1]
+
+
+    def _final_estimate(self, gamma_star, q_hat):
+        TrackTime("Calc beta_hats")
+
+        betas = np.zeros((self.K, self.G))
+        for i in range(len(gamma_star)+1):
+            if i==0:
+                selection = np.arange(self.N)[q_hat <= gamma_star[i][2]]
+            elif i==len(gamma_star):
+                selection = np.arange(self.N)[q_hat > gamma_star[i-1][2]]
+            else:
+                selection = np.arange(self.N)[np.all([q_hat > gamma_star[i-1][2], q_hat <= gamma_star[i][2]],axis=0)]
+
+            x_sel = self.X.loc[selection,:]
+            y_sel = self.Y.loc[selection,:]
+            betas[:,i], _, _, _ = lstsq(x_sel, y_sel)
+
+        col = ['g=%d'%i for i in range(self.G)]
+        row = ['k=%d'%i for i in range(self.K)]
+        self.beta = pd.DataFrame(betas, columns=col, index=row)
 
 
     def estimate_G(self, G):
@@ -70,41 +111,57 @@ class PSEUDO:
         self.T = len(set(self.X.index.get_level_values(1)))
         self.K = len(self.X.columns)
 
-
         q_hat = np.zeros((self.N,self.K))
         for i in range(self.N):
-            q_hat[i,:], _, _, _ = lstsq(self.X.loc[i,:], self.Y.loc[i,:])
+            select = np.arange(i*self.T,(i+1)*self.T)
+            q_hat[i,:], _, _, _ = lstsq(self.X.values[select], self.Y.values[select])
 
         self.min_group_size = 10
+        gamma_stars = [[] for i in range(self.K)]
 
-        self.gamma_star = [np.Inf, 0, 0]
-        TrackTime("Calc gamma_star")
         for k in range(self.K):
-            self._cluster_regressor(k, q_hat[:,k])
+            q_hat_k = q_hat[:,k]
+            for i in range(int(np.ceil(np.log2(self.G)))):
+                if i==0:
+                    gamma_star = self._cluster_regressor(k, q_hat_k, 0)
+                    gamma_stars[k].append(gamma_star)
+                elif 2**(i+1) > self.G:
+                    #TODO: select groups
+                    print("HELLO")
+                else:
+                    left_n = 0
+                    new_gammas = []
+                    for j in range(len(gamma_stars[k])+1):
+                        if j==0:
+                            rel_q_hat = q_hat_k[q_hat_k <= gamma_stars[k][j][2]]
+                        elif j==len(gamma_stars[k]):
+                            rel_q_hat = q_hat_k[q_hat_k > gamma_stars[k][j-1][2]]
+                        else:
+                            rel_q_hat = q_hat_k[np.all([q_hat_k > gamma_stars[k][j-1][2], q_hat_k <= gamma_stars[k][j][2]],axis=0)]
 
-        TrackTime("Calc beta_hat")
-        gamma = self.gamma_star[1]
-        k = self.gamma_star[2]
-        partition = [np.arange(self.N)[q_hat[:,k] <= gamma].reshape(1,-1)[0], np.arange(self.N)[q_hat[:,k] > gamma].reshape(1,-1)[0]]
+                        gamma_star = self._cluster_regressor(k, rel_q_hat, left_n)
+                        new_gammas.append(gamma_star)
+                        left_n += len(rel_q_hat)
 
-        x1 = self.X.loc[partition[0],:]
-        x2 = self.X.loc[partition[1],:]
-        y1 = self.Y.loc[partition[0],:]
-        y2 = self.Y.loc[partition[1],:]
+                    for j in range(len(gamma_stars[k])):
+                        gamma_stars[k][j][0] = 0
+                        gamma_stars[k][j][1] = 0
+                    for j in range(len(gamma_stars[k])+1):
+                        gamma_stars[k].insert(2*j,new_gammas[j])
 
-        beta_hat_1, _, _, _ = lstsq(x1, y1)
-        beta_hat_2, _, _, _ = lstsq(x2, y2)
+        k_star = self._calc_k_star(gamma_stars)
 
-        col = ['g=%d'%i for i in range(self.G)]
-        row = ['k=%d'%i for i in range(self.K)]
-        self.beta = pd.DataFrame(np.vstack([beta_hat_1, beta_hat_2]).T, columns=col, index=row)
+        for gamma in gamma_stars[k_star]:
+            print(gamma[2],end='  ')
+        print("  k:", k_star)
 
+        self._final_estimate(gamma_stars[k_star], q_hat[:,k_star])
 
 
 np.random.seed(0)
 N = 100
-T = 100
-K = 2
+T = 250
+K = 3
 
 TrackTime("Simulate")
 dataset = Dataset(N, T, K, G=2)
@@ -128,10 +185,10 @@ pseudo.fit(x,y)
 TrackTime("Print")
 
 print("TRUE COEFFICIENTS:")
-print(dataset.slopes_df)
+print(dataset.slopes_df.T)
 
 print("\n\nESTIMATED COEFFICIENTS:")
-print(pseudo.beta)
+print(pseudo.beta.T)
 
 
 print("\n")
