23bb03e53faad1387f415d9583ef319f3879ddb5
Marco <m.j.deken@student.vu.nl>
2021-05-03 20:52:34 +0200

    Implemented Conditional K-means algorithm

--- estimate-Lin_Ng-CKmeans.py
+++ estimate-Lin_Ng-CKmeans.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 11 16:02:00 2021
+Created on Mon May 3 17:40:43 2021
 
 @author: Marco
 """
 
 import pandas as pd
 import numpy as np
+from copy import copy
 from scipy.linalg import lstsq
 
 from simulate import Effects, Slopes, Variance, Dataset
@@ -16,6 +17,64 @@ from lib.tracktime import TrackTime, TrackReport
 class CK_means:
     def __init__(self):
         pass
+
+
+    def _initial_values(self):
+        groups = np.random.choice(np.arange(self.G), size=self.N, replace=True)
+
+        self.beta_hat = np.zeros((self.K, self.G))
+        self._calc_beta_hat(groups)
+
+        return groups
+
+
+    def _calc_beta_hat(self, groups):
+        TrackTime("_calc_beta_hat")
+        for g in range(self.G):
+            selection = np.where(groups==g)[0]
+
+            selection_indices = np.zeros(len(selection)*self.T, dtype=int)
+            for i in range(len(selection)):
+                selection_indices[i*self.T:(i+1)*self.T] = np.arange(self.T) + selection[i]*self.T
+
+            x = self.X.values[selection_indices,:]
+            y = self.Y.values[selection_indices]
+
+            self.beta_hat[:,g], _, _, _ = lstsq(x, y)
+        TrackTime("Estimate")
+
+
+    def _ssr(self, i, g, x, y):
+        residuals = y - x @ self.beta_hat[:,g]
+        return residuals@residuals.T
+
+
+    def _ck_means(self):
+        ssr_groups = np.zeros((self.N,self.G))
+        groups = self._initial_values()
+        prev_groups = copy(groups)
+
+        s = 0
+        while True:
+            for i in range(self.N):
+                x = self.X.values[i*self.T:(i+1)*self.T]
+                y = self.Y.values[i*self.T:(i+1)*self.T]
+                for g in range(self.G):
+                    ssr_groups[i,g] = self._ssr(i,g, x, y)
+
+            best_fit = np.min(ssr_groups,axis=1)
+            for g in range(self.G):
+                groups[ssr_groups[:,g] == best_fit] = g
+
+            self._calc_beta_hat(groups)
+
+            if np.all(prev_groups == groups):
+                break
+
+            prev_groups =  copy(groups)
+            s += 1
+
+        return groups, s
 
 
     def estimate_G(self, G):
@@ -30,16 +89,38 @@ class CK_means:
         self.T = len(set(self.X.index.get_level_values(1)))
         self.K = len(self.X.columns)
 
+        best_tot_ssr = np.Inf
+        for k in range(20):
+            groups, s = self._ck_means()
+
+            tot_ssr = 0
+            for i in range(self.N):
+                tot_ssr += self._ssr(i, groups[i], self.X.values[i*T:(i+1)*T], self.Y.values[i*T:(i+1)*T])
+
+            if tot_ssr < best_tot_ssr:
+                best_tot_ssr = tot_ssr
+                best_beta_hat = self.beta_hat
+                self.groups = copy(groups)
+                self.nr_iterations = s
+                print("Iteration %d:\n"%k,np.sort(best_beta_hat, axis=1))
+
+        #TODO: order group numbers based on slope (including fixed effects)
+        self.beta_hat = np.sort(best_beta_hat, axis=1)
+
+        col = ['g=%d'%i for i in range(self.G)]
+        row = ['k=%d'%i for i in range(self.K)]
+        self.beta_hat = pd.DataFrame(self.beta_hat, columns=col, index=row)
+
 
 
 np.random.seed(0)
 N = 250
-T = 100
-K = 1
+T = 50
+K = 2
 
 
 TrackTime("Simulate")
-dataset = Dataset(N, T, K, G=3)
+dataset = Dataset(N, T, K, G=7)
 dataset.simulate(Effects.ind_fix, Slopes.heterog, Variance.homosk)
 
 
@@ -56,11 +137,13 @@ ck_means.fit(x,y)
 
 TrackTime("Print")
 
-print("\n\nTRUE COEFFICIENTS:")
+print("TOOK %s ITERATIONS\n"%ck_means.nr_iterations)
+
+print("TRUE COEFFICIENTS:")
 print(dataset.slopes_df)
 
 print("\n\nESTIMATED COEFFICIENTS:")
-# print(ck_means.beta)
+print(ck_means.beta_hat)
 
 
 # from linearmodels import PanelOLS
